1abad29e4bd5fe6cc90c3d0d9520e6b8cfaf08fe
Rahul Bhalley <0behinds_secant@icloud.com>
2026-03-06 09:09:22 +0530

    Data: Update pipeline with infinite loaders and add DiffAugment

--- data.py
+++ data.py
@@ -1,71 +1,56 @@
 # -*- coding: utf-8 -*-
 
-__author__ = "Rahul Bhalley"
-
-from config import *
-
+from config import config
+import itertools
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 import torchvision.datasets as dsets
 
-
 def load_data():
-
     # Preprocessing
     transform = transforms.Compose([
         transforms.RandomHorizontalFlip(),
-        transforms.Resize(LOAD_DIM),
-        transforms.RandomCrop(CROP_DIM),
+        transforms.Resize(config.LOAD_DIM),
+        transforms.RandomCrop(config.CROP_DIM),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5] * 3, std=[0.5] * 3)
     ])
 
     # Make datasets
-    X_folder = dsets.ImageFolder(DATASET_PATH["trainA"], transform=transform)
-    Y_folder = dsets.ImageFolder(DATASET_PATH["trainB"], transform=transform)
+    X_folder = dsets.ImageFolder(config.DATASET_PATH["trainA"], transform=transform)
+    Y_folder = dsets.ImageFolder(config.DATASET_PATH["trainB"], transform=transform)
     
-    # Make dataset loaders
-    X_set = DataLoader(X_folder, batch_size=BATCH_SIZE, shuffle=True)
-    Y_set = DataLoader(Y_folder, batch_size=BATCH_SIZE, shuffle=True)
+    # Make dataset loaders with drop_last=True to handle uneven batch sizes
+    X_loader = DataLoader(
+        X_folder, 
+        batch_size=config.BATCH_SIZE, 
+        shuffle=True, 
+        drop_last=True,
+        num_workers=2,
+        pin_memory=True
+    )
+    Y_loader = DataLoader(
+        Y_folder, 
+        batch_size=config.BATCH_SIZE, 
+        shuffle=True, 
+        drop_last=True,
+        num_workers=2,
+        pin_memory=True
+    )
     
     # Print length of sample batches
     print("Dataset Details")
-    print(f"X_set batches: {len(X_set)}")
-    print(f"Y_set batches: {len(Y_set)}")
+    print(f"X_set batches: {len(X_loader)}")
+    print(f"Y_set batches: {len(Y_loader)}")
     print("")
 
-    # Return the datasets
-    return X_set, Y_set
+    # Return infinite iterators
+    return itertools.cycle(X_loader), itertools.cycle(Y_loader)
 
-def get_infinite_X_data(X_set):
-    while True:
-        for x, _ in X_set:
-            yield x
-
-def get_infinite_Y_data(Y_set):
-    while True:
-        for y, _ in Y_set:
-            yield y
-
-# There's some problem with batch size 
-# of sampled data using `torchvision`.
-# 
-# This block of code tries to 
-# eliminate the problem.
-
-def safe_sampling(X_data, Y_data, device):
-
-    # First sample the data
-    x_sample, y_sample = next(X_data), next(Y_data)
+def safe_sampling(X_iter, Y_iter, device):
+    # Sample the data
+    x_sample, _ = next(X_iter)
+    y_sample, _ = next(Y_iter)
     
-    # Check requirement conditions
-    # and sample next accordingly.
-    if x_sample.size(0) != BATCH_SIZE:  # condition for `x_sample`
-        print(f"Batch size not equal to that of x_sample: {BATCH_SIZE} != {x_sample.size(0)} | skipping...")
-        x_sample = next(X_data)
-    if y_sample.size(0) != BATCH_SIZE:  # condition for `y_sample`
-        print(f"Batch size not equal to that of y_sample: {BATCH_SIZE} != {y_sample.size(0)} | skipping...")
-        y_sample = next(Y_data)
-    
-    # Return correct data
-    return x_sample.to(device), y_sample.to(device)
+    # Return correct data with channels_last if possible (handled in main usually, but can be done here)
+    return x_sample.to(device, non_blocking=True), y_sample.to(device, non_blocking=True)
